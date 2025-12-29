c82740848c85a800bae60fb700a852b7795f2c22
anubhab001 <49355634+anubhab001@users.noreply.github.com>
2025-12-29 12:26:15 +0900

    refactoring

---
+++ optimised-software/fix-slice/arm32/sbox_comparison/sbox.h
@@ -0,0 +1,18 @@
+#ifndef sbox_H_
+#define sbox_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+extern void test_baksheesh(uint32_t* in,  uint32_t* out);
+extern void test_gift(uint32_t* in,  uint32_t* out);
+extern void test_piccolo(uint32_t* in,  uint32_t* out);
+extern void test_present(uint32_t* in,  uint32_t* out);
+extern void test_pyjamask128(uint32_t* in,  uint32_t* out);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
